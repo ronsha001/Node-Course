c50487baf9d24a1ec8bfc083316be4dc126672e5
ronsha001 <ronsh0111@gmail.com>
2022-07-17 17:01:16 +0300

    Working with REST APIs - The Basics

--- section24 - Working with REST APIs - The Basics/app.js
+++ section24 - Working with REST APIs - The Basics/app.js
@@ -1,125 +1,22 @@
-const path = require("path");
-
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const session = require("express-session");
-const MongoDBStore = require("connect-mongodb-session")(session);
-const csrf = require("csurf");
-const flash = require("connect-flash");
-const dotenv = require("dotenv");
-const multer = require("multer");
-const uniqId = require('uniqid');
 
-const errorController = require("./controllers/error");
-const User = require("./models/user");
+const feedRoutes = require("./routes/feeds");
 
 const app = express();
-dotenv.config();
 
-const MONGODB_URI = process.env.MONGODB_URI;
+app.use(bodyParser.urlencoded({ extended: false })); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
 
-const store = new MongoDBStore({
-  uri: MONGODB_URI,
-  collection: "sessions",
-});
-const csrfProtection = csrf();
-
-const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, uniqId() + "-" + file.originalname);
-  },
-});
-
-const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype === "image/png" ||
-    file.mimetype === "image/jpg" ||
-    file.mimetype === "image/jpeg"
-  ) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
-
-app.set("view engine", "ejs");
-app.set("views", "views");
-
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-const authRoutes = require("./routes/auth");
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
-);
-app.use(express.static(path.join(__dirname, "public")));
-app.use('/images', express.static(path.join(__dirname, "images")));
-
-app.use(
-  session({
-    secret: "my secret",
-    resave: false,
-    saveUninitialized: false,
-    store: store,
-  })
-);
-app.use(csrfProtection);
-app.use(flash());
-
+// CORS stand for Cross-Origin Resource Sharing
+// Prevent CORS Error by setting headers
 app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
-app.use((req, res, next) => {
-  if (!req.session.user) {
-    return next();
-  }
-  User.findById(req.session.user._id)
-    .then((user) => {
-      if (!user) {
-        return next();
-      }
-      req.user = user;
-      next();
-    })
-    .catch((err) => {
-      next(new Error(err));
-    });
-});
+app.use("/feed", feedRoutes);
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
-app.use(authRoutes);
-// app.get('/:imagePath', (req, res, next) => {
-//   const imagePath = req.params.imagePath;
-//   console.log(imagePath);
-//   next();
-// })
-app.use("/500", errorController.get500);
-
-app.use(errorController.get404);
-
-app.use((error, req, res, next) => {
-  console.log(error);
-  res.status(500).render("500", {
-    pageTitle: "Error!",
-    path: "/500",
-    isAuthenticated: req.session.isLoggedIn,
-  });
-});
-
-mongoose
-  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => {
-    app.listen(3000);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+app.listen(8080);
---
+++ section24 - Working with REST APIs - The Basics/controllers/feed.js
@@ -0,0 +1,18 @@
+exports.getPosts = (req, res, next) => {
+  res.json({
+    posts: [
+      { title: "First Posts", content: "This is the first post!" },
+      { title: "Second Post", content: "This is the second post!" },
+    ],
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  // Create post in db
+  res.status(201).json({
+    message: "Post created successfully!",
+    post: { id: new Date().toISOString(), title: title, content: content },
+  });
+};
